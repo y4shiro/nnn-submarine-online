d2404d91cfa45f551437aa64ee27bdb14fe56c6e
y4shiro <y4shironao@gmail.com>
2020-12-25 21:13:41 +0900

    アイテムとプレーヤーの配置処理

--- game_server/webSocketServer.js
+++ game_server/webSocketServer.js
@@ -7,12 +7,16 @@ function createWebSocketServer(io, game) {
 
     console.log('WebSocket のコネクションがありました。');
     socket.emit('start data', {});
+    const startObj = game.newConnection(socket.io, displayName, thumbUrl);
+    socket.emit('start data', startObj);
 
-    socket.on('disconnect', () => {});
+    socket.on('disconnect', () => {
+      game.disconnect(socket.id);
+    });
   });
 
   const socketTicker = setInterval(() => {
-      rootIo.volatile.emit('map data', {}); // 全員に送信
+      rootIo.volatile.emit('map data', game.getMapData()); // 全員に送信
     }, 66);
 }
 
